8c5d838620ae1d8be6f97f0cc4e2f59751afe70c
robbijamal <robbijamal@gmail.com>
2022-03-19 09:20:39 +0700

    menambahkan creator di MenuController

--- app/Http/Controllers/API/Menucontroller.php
+++ app/Http/Controllers/API/Menucontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Menu;
-
+//Creator : Sarjono Edi Prawiro
 class Menucontroller extends Controller
 {
     /**
